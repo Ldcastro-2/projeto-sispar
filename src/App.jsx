d9a5e3e4e95b1c2f8fcde910dd41bb8b7c859616
Ldcastro-2 <lucianade.castro2@gmail.com>
2025-03-11 17:54:35 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {HushRouter, Routes, Route, BrowserRouter } from "react-router-dom";
+import {BrowseRouter, Routes, Route } from "react-router-dom";
 import Login from "./Components/login/Login.jsx";
 import Reembolsos from "./Components/reembolsos/Reembolsos.jsx";
 import Solicitacao from "./Components/solicitacao/Solicitacao.jsx";
@@ -8,13 +8,13 @@ import "./global.scss";
 
 function App() {
   return (
-    <HushRouter>
+    <BrowseRouter>
       <Routes>
         <Route path="/" element={<Login/>} /> 
         <Route path="/reembolso" element={<Reembolsos/>} />
         <Route path="/solicitacao" element={<Solicitacao/>}/>
       </Routes>
-    </HushRouter>
+    </BrowseRouter>
   );
 }
 
